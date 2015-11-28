4222e09bc2704cb06803ddd5375a90cc661516d6
flodel <flodel@gmail.com>
2015-11-28 07:47:45 -0500

    found and fixed one bad record where the "Data Table Name" was missing a colon right after.

--- parse_fld_def_pdf.r
+++ parse_fld_def_pdf.r
@@ -6,14 +6,18 @@ pdfParsed <- pdfReader(elem = list(uri = uri),
                        language = "en",
                        id = "id1")
 
+flddefs<-data.frame(name=character(),table=character(),category=character(),type=character(),
+                    percentrank=character(),ismedian=character(),desc=character(),stringsAsFactors = FALSE)
+
 data <- pdfParsed$content
 data <- gsub("\xad", "-", data) # fix some weird dashes
 data <- gsub("\xae", "-", data) # remove copyright character
 data <- data[!grepl("Stock Investor Pro Field Definitions", data)] # remove page header
 data <- data[data != ""] # remove blank lines
+data <- sub("Data Table Name ", "Data Table Name:", data) # fixes one bad record
 
 # identify the blocks of data defining each field and split into a list
-table_idx <- grep("Data Table Name:", data)
+table_idx <- grep("Data Table Name[:]?", data)
 name_idx  <- table_idx - 1L
 stopifnot(1L %in% name_idx)
 is_new_field <- seq_along(data) %in% name_idx
@@ -22,11 +26,12 @@ field_list <- split(data, field_idx)
 
 # a function to check our assumptions for the data of each field
 is_valid_field <- function(dat) {
-   grepl("Data Table Name:",            dat[2]) &
+   grepl("Data Table Name:",         dat[2]) &
    grepl("Data Category:",              dat[3]) &
    grepl("Field Type:",                 dat[4]) &
    grepl("Percent Rank:",               dat[5]) &
-   grepl("(Industry|Company)/Sector Median[s]?:", dat[6])
+   grepl("(Industry|Company)/Sector Median[s]?:", dat[6]) &
+   !any(grepl("Data Table Name", tail(dat, -6)))
 }
 bad_data <- Filter(Negate(is_valid_field), field_list)
 if (length(bad_data) > 0L) {print(bad_data); stop("BAD DATA!!! See above")}
