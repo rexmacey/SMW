28c52304b32679f6d46343f4ef601e0247ec6338
Rex Macey <rex@macey.us>
2015-12-21 10:29:27 -0500

    Fixed the convert_1install_dbf_to_rds function so duplicate COMPANY_IDs are eliminated. Cleaned up code.

--- convertdbf2rdata.r
+++ convertdbf2rdata.r
@@ -1,34 +1,17 @@
-# Convert data from AAII's dbf files into r's rdata files.
+# Convert data from AAII's dbf files into r's rds files.
 # When new AAII data file is loaded, it should be installed into a subfolder named SIPYYYYMMDD under the mainfolder as defined below
-# Append an entry to sipbInstallDates which is in the working directory and save the .rdata file.
+# Append an entry to sipbInstallDates which is in the working directory and save the .rds file.
 # Run convertmostrecentdbf2rdata function
-
-## FD: Comments with a double pounds and "FD" (Florent Delmotte) are for code review only, feel free to delete after review.
 
 library(foreign)
 setwd("C:/Users/Rex/Documents/Quant Trading/SMW")
 source("SMWutilities.r")
 init_environment()
-mainfolder <- "D:/SIPro"
 
 convertmostrecentdbf2rdata<-function(){
     strDate <- tail(sipbInstallDates, 1)    ## FD: use tail() last element.
-    convert_1install_dbf_to_rdata(strDate)
+    convert_1install_dbf_to_rds(strDate)
     convert_price_files_to_rdata(strDate)
-}
-
-
-
-## FD: Recommend you never use T or F as shortcuts for TRUE and FALSE since T and F can be overwritten
-##     e.g. your code could do a lot of weird and hard to debug things if your code had T <- 0 or F <- 1 somewhere
-##     Instead, TRUE and FALSE are built-in constants so they can't be overwritten.
-
-loadfile<-function(fn, folder, prnt=FALSE, flds=NULL){
-    # loads the fields (all fields if flds==NULL) from dbf fn in folder
-    data <- read.dbf(file = paste0(file.path(folder, fn),".dbf"), as.is = TRUE)
-    if (!is.null(flds)) data <- data[, flds, drop = FALSE]
-    if (prnt) print(str(data))
-    return(data)
 }
 
 tonumeric<-function(data,varnames){
@@ -54,55 +37,6 @@ convert_all_price_files_to_r<-function(){  #convert all files
     lapply(sipbInstallDates,convert_price_files_to_rdata)
 }
 
-convert_1install_dbf_to_rdata<-function(strDate){  #all files except price files
-    sipfolder <- dbflocations(strDate)
-
-    ## FD: This will be a lot easier to maintain. It could stay here or go into
-    ##     a csv config file.
-    ##     I included a NEED_CLEAN column because you apparenlty did not
-    ##     use cleanfile on ss_ci. I don't know if this was a mistake or intentional.
-    to_process <- read.table(text =
-      "NAME     DIR       NEED_CLEAN
-       ci       static    FALSE
-       mlt      dbfs      TRUE
-       isq      static    TRUE
-       gr       dbfs      TRUE
-       perc     dbfs      TRUE
-       rat      dbfs      TRUE
-       psd      dbfs      TRUE
-       isa      static    TRUE
-       ee       dbfs      TRUE
-       bsa      static    TRUE
-       bsq      static    TRUE
-       val      dbfs      TRUE
-       cfa      static    TRUE
-       cfq      static    TRUE", header = TRUE, stringsAsFactors = FALSE)
-
-    files <- sprintf("si_%s", to_process$NAME)
-    dirs  <- sipfolder[to_process$DIR]
-    rdata <- sprintf("%s/rdata/si_%s_%s.rdata", mainfolder, to_process$NAME, strDate)
-    need_clean <- to_process$NEED_CLEAN
-
-    ## FD: a for loop will be more memory efficient
-    for (i in seq_along(files)) {
-      data <- loadfile(files[i], dirs[i])
-      if (need_clean[i]) data <- cleanfile(data)
-      save(data, file = rdata[i])
-    }
-}
-
-convert_all_dbf_files_to_r<-function(){  #convert all files
-    library(foreign)
-    setwd("C:/Users/Rex/Documents/Quant Trading/SMW")
-    source("SMWutilities.r")
-    init_environment()
-    mainfolder <- "D:/SIPro"
-    # lapply(sipbInstallDates,convert_1install_dbf_to_rdata)  #uses too much memory
-    for (i in sipbInstallDates){
-        convert_1install_dbf_to_rdata(i)
-    }
-}
-
 cleanfile<-function(df){
     XNullCol<-match("X_NullFlags",colnames(df))
     if (! is.na(XNullCol)){
@@ -114,7 +48,8 @@ cleanfile<-function(df){
     return(df)
 }
 
-convert_1install_dbf_to_rds<-function(strDate){ 
+convert_1install_dbf_to_rds<-function(strDate){
+    blndebug<-FALSE
     print(strDate)
     sipfolder <- dbflocations(strDate)
     to_process <- read.table(text =
@@ -140,14 +75,37 @@ convert_1install_dbf_to_rds<-function(strDate){
     dirs  <- sipfolder[to_process$DIR]
     need_clean <- to_process$NEED_CLEAN
     out <- read.dbf(file = paste0(file.path(dirs[1], files[1]),".dbf"), as.is = TRUE)
+    if (blndebug) {
+        print("DBF#: 1 Top and Bottom COMPANY_IDs")
+        print(head(out$COMPANY_ID))
+        print(tail(out$COMPANY_ID))
+        print(paste0("DBF#: ",1," file: ",dirs[1],"/",files[1]," data rows: ",nrow(out)))
+        message("number of duplicates: ", sum(duplicated(out$COMPANY_ID)))
+    }
     if (need_clean[1]) data <- cleanfile(data)
     for (i in 2:length(files)) {
       data <- read.dbf(file = paste0(file.path(dirs[i], files[i]),".dbf"), as.is = TRUE)
+      if (blndebug) {
+          print(paste("DBF#:",i," Top and Bottom COMPANY_IDs"))
+          print(head(data$COMPANY_ID))
+          print(tail(data$COMPANY_ID))
+          print(paste0("DBF#: ",i," file: ",dirs[i],"/",files[i]," data rows: ",nrow(data)))
+      }
+      idx<-apply(is.na(data),2,sum)==nrow(data)
+      data<-data[!idx]
       if (need_clean[i]) data <- cleanfile(data)
       out<-merge(out,data,by="COMPANY_ID")
+      if (blndebug) print(paste0("out rows after merge: ",nrow(out)))
+      if (blndebug) message("number of duplicates: ", sum(duplicated(out$COMPANY_ID)))
     }
-    idx<-apply(is.na(out),2,sum)==nrow(out)
+    
+    # remove dupes
+    dup.idx<-duplicated(out$COMPANY_ID)
+    out<-out[!dup.idx,]
+    idx<-apply(is.na(out),2,sum)==nrow(out) # get rid of NA columns
     out<-out[!idx]
+    idx<-grep("UPDATE",colnames(out))
+    if (length(idx)>0) out<-out[-idx]
     for (x in c("IND_2_DIG","IND_3_DIG","SP","EXCHANGE","STATE","COUNTRY","DOW")){
         out[,x] <- factor(out[,x])
     }
@@ -162,11 +120,18 @@ convert_1install_dbf_to_rds<-function(strDate){
     idx <- out$PRICE>=5 & out$MKTCAP >=250
     out<-out[idx,]
     saveRDS(out,file=paste0(rdata.folder,"sip_",strDate,".rds"))
-    return(NULL)
+    return(out)
+}
+
+convert_all_dbf_files_to_rds<-function(){  #convert all files
+    for (i in sipbInstallDates){
+        print(i)
+        data<-convert_1install_dbf_to_rds(i)
+    }
 }
 
 open_all_dbfs<-function(i=1){
-    # utility function to open all dbfs, so one can inspect various data
+    # utility function to open all dbfs for one date, so one can inspect various data
     out<-list()
     strDate<-sipbInstallDates[i]
     folders_list<-c("/Dbfs/","/Static/")
